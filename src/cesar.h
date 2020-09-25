5a4cb174016087d0de6643c98c71206c76fa3c05
stepin104252 <69074637+stepin104252@users.noreply.github.com>
2020-09-25 17:59:12 +0530

    Create cesar.h

---
+++ src/cesar.h
@@ -0,0 +1,13 @@
+#ifndef __CESAR_H
+#define __CESAR_H
+
+/**
+ * \fn int main(int argc,char** argv)
+ * \brief fonction main
+ * \param argc est le nombre de mots dans les commandes
+ * \param argv est le tableau des chaines de caractères issues des commandes dans le terminal
+ * \return 0 : arrêt normal du programme
+ * */
+int main(int argc, char **argv);
+
+#endif
